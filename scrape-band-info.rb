9ef183935266dc557744cc25437f8b5fed76db41
Evan <eulloyd@gmail.com>
2014-03-04 06:58:57 +0000

    Add band info scraper

---
+++ scrape-band-info.rb
@@ -0,0 +1,21 @@
+require 'open-uri'
+require 'nokogiri'
+require 'csv'
+
+file = File.open('list.txt', 'r')
+CSV.open('band.info.csv', 'a+')
+while line = file.gets #is this right?
+  page = Nokogiri::HTML(open("#{line}"))
+  @name = page.css('h1').text
+  @english_name = page.css('.inner')[1].text
+  @label = page.css('.inner')[3].text
+  @contact = page.css('.inner')[5].text
+  @genre = page.css('.inner')[7] #this is broken
+  @facebook = page.css('.button.float-left')[0]['href']
+  puts @name
+  puts @english_name
+  puts @label
+  puts @contact
+  puts @genre
+  puts @facebook
+end
